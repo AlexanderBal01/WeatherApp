d7359417c6b321dc2bc3937e2bfc766a6fe32d2a
Alexander Bal <alexander.bal@student.hogent.be>
2024-03-27 11:03:55 +0100

    added function for the homescreen composable

---
+++ app/src/main/java/com/example/weather/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.weather.ui.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+}
